75eddfbab153140b69e5f333227313b256f923d8
kimberlyrojasra <kimberlyrojasra@gmail.com>
2020-01-18 19:07:21 -0500

    arquitectura y dependencias, se deployo en now

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header/Heder';
+
+const App = () => (
+  <>
+    <Header />
+    <div className="App">
+      <p>
+        Editand save to reload.
+      </p>
+    </div>
+  </>
+);
+
+export default App;
